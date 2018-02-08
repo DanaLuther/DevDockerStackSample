c9e7a4379eb930dd1187ee90c911058a4df58ee1
Dana Luther <dluther@envisageinternational.com>
2018-02-08 15:58:25 -0500

    Added index.php with illustration of the php version

---
+++ public_html/index.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <title>Converting Your DEV Environment to a Docker Stack</title>
+</head>
+<body>
+<div class="jumbotron text-center">
+<h1>Hello, SunshinePHP!</h1>
+<img src="/img/horizontal.png" /> <br />
+    PHP Version: <?= phpversion( ); ?>
+</div>
+
+<div class="text-center">
+    <a href="phpinfo.php" class="btn btn-lg btn-primary">Click here to check the php settings</a>
+</div>
+</body>
+</html>
