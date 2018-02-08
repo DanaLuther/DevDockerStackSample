f78e3be7c03d68db3ef4c369637e2f4e18a82151
Dana Luther <dluther@envisageinternational.com>
2018-02-08 17:00:22 -0500

    Added MySQL version indicator Added MySQL PHP Extension integration optional compose file Added samples of customized php base image files

---
+++ php-image-options/Readme.md
@@ -0,0 +1,6 @@
+# Building Your Own Image
+
+PHP 7.0 FPM w/ MySQL
+```docker image build -f Dockerfile-7-fpm -t dhluther/php:7-fpm-mysql .```
+
+```docker image build -f Dockerfile-7-fpm-all-the-goodies -t dhluther/php:7-fpm-all-the-goodies .```
--- public_html/index.php
+++ public_html/index.php
@@ -2,14 +2,26 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <title>Converting Your DEV Environment to a Docker Stack</title>
 </head>
 <body>
 <div class="jumbotron text-center">
-<h1>Hello, SunshinePHP!</h1>
-<img src="/img/horizontal.png" /> <br />
-    PHP Version: <?= phpversion( ); ?>
+    <h1>Hello, SunshinePHP!</h1>
+    <img src="/img/horizontal.png"/> <br/>
+    PHP Version: <?= phpversion(); ?><br/>
+	<?php
+	if ( extension_loaded( 'mysqli' ) )
+	{
+		$db = new \mysqli( 'db', 'root', 'sunshinephp' );
+		echo "MySQL Version: " . $db->server_version;
+	} else
+	{
+		echo '<span class="text-danger">MySQL extension is not currently enabled</span>';
+
+	}
+	?>
 </div>
 
 <div class="text-center">
