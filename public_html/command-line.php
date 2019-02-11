d6bbcd27f1ab4285efeddc9750c4a9b2c6a312c4
Dana Luther <dluther@envisageinternational.com>
2019-02-11 08:44:55 -0500

    Remove auto-generated doc stub

--- public_html/command-line.php
+++ public_html/command-line.php
@@ -3,7 +3,6 @@
  * command-line.php
  *
  * @author danaluther  <dluther@envisageinternational.com>
- * @copyright (c) Envisage International, Corp
  * @created 10/16/18 12:15 PM
  */
 echo PHP_EOL, PHP_EOL, 'Hello SunshinePHP!',  PHP_EOL, PHP_EOL;
